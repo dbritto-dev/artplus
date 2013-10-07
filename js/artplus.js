d8e5e4b1e061fe0ca8a145cea71a757ac7b71340
danilobrinu <ddbn.c2@gmail.com>
2013-10-07 18:23:07 -0500

    subiendo mas cambios y plantilla para el blog

--- js/artplus.js
+++ js/artplus.js
@@ -1,18 +1,25 @@
 // user transitions to animation, animation is better
 var menu_aside_show = true;
-$(window).scroll(function() {
-	if ($(window).height() <= $(window).scrollTop()) {
-		if (menu_aside_show) {
-			$('.body__content__sections, .header').css({position: 'relative'});
-			$('.body__menu').toggleClass('body__menu--active');
+if ($('.header').length >0) {
+	$(window).scroll(function() {
+		
+		if ($(window).height() <= $(window).scrollTop()) {
+			if (menu_aside_show) {
+				$('.body__content__sections__section--design, .header').css('position', 'relative');
+				$('.body__content__sections__section--architecture').css('marginTop', '0');
+				$('.body__menu').toggleClass('body__menu--active');
+			}
+			menu_aside_show = false;
+		} else {
+			if (!menu_aside_show) {
+				$('.body__content__sections__section--design').css('position', 'fixed');
+				$('.header').css('position', 'absolute');
+				$('.body__content__sections__section--architecture').css('marginTop', '200%');
+				$('.body__menu').toggleClass('body__menu--active');
+			}
+			menu_aside_show = true;			
 		}
-		menu_aside_show = false;
-	} else {
-		if (!menu_aside_show) {
-			$('.header').css({position: 'absolute'});
-			$('.body__menu').toggleClass('body__menu--active');
-			$('.body__content__sections').css({position: 'fixed'});
-		}
-		menu_aside_show = true;			
-	}
-});
+	});
+} else {
+	$('.body__menu').addClass('body__menu--active');
+}
